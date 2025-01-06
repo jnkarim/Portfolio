d22ade62788cbedd959d63a525eb0c815e11ea44
Julker Nayeen Karim <151433912+jnkarim@users.noreply.github.com>
2025-01-07 00:10:31 +0600

    Update Bio.jsx

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -6,7 +6,7 @@ const Bio = () => {
     <div className="flex flex-col lg:flex-row items-center lg:items-start gap-8 px-4 lg:px-16">
       <div className="w-full lg:w-1/2">
         <div className="flex flex-col items-center lg:items-start">
-          <h2 className="pb-8 text-5xl font-semibold tracking-tight">
+          <h2 className="pb-8 text-5xl font-semibold tracking-tight text-center">
             Glad to see you landing here!
           </h2>
           <span className="bg-gradient-to-r from-green-300 via-slate-500 to-blue-500 bg-clip-text text-4xl tracking-tight text-transparent text-center">
